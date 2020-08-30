c35f1b107eaa947a2c2c42aa16c2167491325535
David Tkeshelashvili <dtkeshelashvili07@gmail.com>
2020-08-31 00:45:53 +0400

    create big canvas

--- scipt.js
+++ scipt.js
@@ -1,12 +1,12 @@
 function setup() {
-	createCanvas(300, 500);
+	createCanvas(600, 1000);
 	background(220, 100, 50);
 }
 
 function draw() {
 	noStroke();
 	fill(200, 180, 250, 70);
-	ellipse(mouseX, mouseY, 20);
+	ellipse(mouseX, mouseY, 50);
 }
 
 function mouseClicked() {
